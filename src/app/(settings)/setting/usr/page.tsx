3cb3f05b9c7f39129ecbdb61090ff1a830f6d1cd
Sooryanarayana Hebbar <Sooryanarayana.Hebbar@endava.com>
2026-05-07 17:33:07 +0530

    setting/usr/page impl

--- src/app/(settings)/setting/usr/page.tsx
+++ src/app/(settings)/setting/usr/page.tsx
@@ -1,25 +1,12 @@
 "use client";
 
-import { DataTableV2 } from '@/components/ui/DataTableV2/DataTable';
+import UserSetting from '@/components/ui/settings/UserSettings';
 import { fetchMockData, UserData } from '@/services/mockService';
 import { useEffect, useState } from 'react';
 
-export default function RegistryV2Page() {
+export default function UserSettingFeaturesPage() {
   const [data, setData] = useState<UserData[]>([]);
   const [loading, setLoading] = useState(true);
-
-  // Overriding ONLY the status column to show a Badge
-  const myOverrides = [
-    {
-      accessorKey: "status",
-      header: "Current Status",
-      cell: (info: any) => (
-        <span className="px-2 py-1 bg-blue-100 text-blue-700 rounded-full text-[10px] font-bold">
-          {info.getValue()}
-        </span>
-      )
-    }
-  ];
 
     useEffect(() => {
       const loadData = async () => {
@@ -37,10 +24,7 @@ export default function RegistryV2Page() {
 
   return (
     <div className="p-6">
-      <DataTableV2
-        data={data} 
-        overrideColumns={myOverrides} // Optional! If removed, it auto-generates 'status'
-      />
+      <UserSetting></UserSetting>
     </div>
   );
 }
